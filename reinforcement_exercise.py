c5a84012105953e3db5f5090f1a6bc1a0b236bfd
Brody Currie <brodycurrie@gmail.com>
2019-05-08 14:52:10 -0400

    added extra functionality

--- reinforcement_exercise.py
+++ reinforcement_exercise.py
@@ -1,32 +1,32 @@
-
-
 def grade(percentage):
-    if percentage == 100 or percentage >= 95:
+    if percentage <= 100 and percentage >= 95:
         return 'A'
-    elif percentage == 94 or percentage >= 87:
+    elif percentage <= 94 and percentage >= 87:
         return 'A'
-    elif percentage == 86 or percentage >= 80:
+    elif percentage <= 86 and percentage >= 80:
         return 'A-'
-    elif percentage == 79 or percentage >= 77:
+    elif percentage <= 79 and percentage >= 77:
         return 'B+'
-    elif percentage == 76 or percentage >= 73:
+    elif percentage <= 76 and percentage >= 73:
         return 'B'
-    elif percentage == 72 or percentage >= 70:
+    elif percentage <= 72 and percentage >= 70:
         return 'B-'
-    elif percentage == 69 or percentage >= 67:
+    elif percentage <= 69 and percentage >= 67:
         return 'C+'
-    elif percentage == 66 or percentage >= 63:
+    elif percentage <= 66 and percentage >= 63:
         return 'C'
-    elif percentage == 62 or percentage >= 60:
+    elif percentage <= 62 and percentage >= 60:
         return 'C-'
-    elif percentage == 59 or percentage >= 57:
+    elif percentage <= 59 and percentage >= 57:
         return 'D+'
-    elif percentage == 56 or percentage >= 53:
+    elif percentage <= 56 and percentage >= 53:
         return 'D'
-    elif percentage == 52 or percentage >= 50:
+    elif percentage <= 52 and percentage >= 50:
         return 'D-'
-    else:
+    elif percentage <= 49:
         return 'R'
+    else:
+        return 'Invalid Input!'
 
 grade_in_percent = int(input("enter your percentage: "))
 
