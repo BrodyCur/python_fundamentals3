b8db077e4e0beaa3d9164feae3a48b2b56bd69e1
Brody Currie <brodycurrie@gmail.com>
2019-05-08 15:32:09 -0400

    finished part 0 of exercises

---
+++ exercises1.py
@@ -0,0 +1,16 @@
+fav_colours = ['orange', 'purple', 'blue', 'pink']
+ages = [35, 34, 27, 22]
+coin_flip = ['tails', 'heads', 'tails', 'tails', 'tails']
+music_artists = ['Owen Pallet', '2mello', 'Macross 1988']
+
+words = {
+    'blossom': "to flourish; develop",
+    'heaven': "the abode of God, the angels, and the spirits of the righteous after death; the place or state of existence of the blessed after the mortal life",
+    'proud': "feeling pleasure or satisfaction over something regarded as highly honorable or creditable to oneself"
+}
+
+print(fav_colours)
+print(ages)
+print(coin_flip)
+print(music_artists)
+print(words)
