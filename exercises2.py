1b4528946c5261681ffab36d1c548ef7bd523bc1
Brody Currie <brodycurrie@gmail.com>
2019-05-09 14:07:03 -0400

    finished collections and interation part 2

--- exercises2.py
+++ exercises2.py
@@ -1,25 +1,63 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+# Exercise 9
 
-def output_list(list):
-    for item in list:
-        print(f"* {item}")
-    print(len(list))
-    if item in list == "bananas":
-        print("You need to pick up bananas.")
-    else:
-        print("You don't need bananas today.")
+# grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+# def output_list(list):
+#     for item in list:
+#         print(f"* {item}")
+#     print(len(list))
+#     if item in list == "bananas":
+#         print("You need to pick up bananas.")
+#     else:
+#         print("You don't need bananas today.")
     
-    print(list[1])
+#     print(list[1])
 
 
-grocery_list.append('rice')
+# grocery_list.append('rice')
 
-output_list(grocery_list)
+# output_list(grocery_list)
 
-grocery_list.sort()
+# grocery_list.sort()
 
-output_list(grocery_list)
+# output_list(grocery_list)
 
-grocery_list.remove('salmon')
+# grocery_list.remove('salmon')
 
-output_list(grocery_list)
+# output_list(grocery_list)
+
+
+# Exercise 10
+
+students = {
+    "cohort1": 34,
+    "cohort2": 42,
+    "cohort3": 22
+}
+
+def display_cohorts(dict):
+    for key, value in dict.items():
+        value = value * 1.05
+        print(f"{key}: {value:.0f}")
+        
+
+    print(dict.keys())
+
+students['cohort4'] = 43
+
+def cohort_calculator(dict):
+    total = 0
+    for key, value in dict.items():
+        total += value
+
+    print(total)
+
+display_cohorts(students)
+cohort_calculator(students)
+
+
+students.pop('cohort2')
+
+
+display_cohorts(students)
+cohort_calculator(students)
