eff01f5278e5a4eea43f25bee33d84fcaefcd110
Brody Currie <brodycurrie@gmail.com>
2019-05-09 13:34:57 -0400

    finished exercise 9

--- exercises2.py
+++ exercises2.py
@@ -8,7 +8,18 @@ def output_list(list):
         print("You need to pick up bananas.")
     else:
         print("You don't need bananas today.")
+    
+    print(list[1])
+
 
 grocery_list.append('rice')
+
+output_list(grocery_list)
+
+grocery_list.sort()
+
+output_list(grocery_list)
+
+grocery_list.remove('salmon')
 
 output_list(grocery_list)
